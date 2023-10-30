d6b72da40f67b72516f9ff60b7cec29a0a708908
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2023-10-30 08:38:19 -0600

    fix some issues

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -2,53 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BlogPost;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use OpenAI\Laravel\Facades\OpenAI;
+use App\Jobs\RequestBlogPost;
 
 class BlogPostController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $result = '';
-        $result = OpenAI::chat()->create([
-            'model' => 'gpt-3.5-turbo',
-            'messages' => [
-                ['role' => 'system', 'content' => 'Generate an article about a feature of laravel. Provide your answer in markdown form. Reply with only the answer in markdown form. Add Extra Line breaks between paragraphs. Use code examples in markdown when needed.'],
-            ],
+        RequestBlogPost::dispatch();
+
+        return response()->json([
+            'message' => 'success',
         ]);
-
-        $result = $result['choices'][0]['message']['content'];
-
-        $post = new BlogPost();
-
-        $subject = '';
-
-        $subject = $this->parseSubject($result);
-
-        $subject = str_replace(['#', '*'], '', $subject);
-
-        $result = str_replace($subject, '', $result);
-
-        $data = [
-            'user_id' => 1,
-            'subject' => $subject,
-            'body' => $result,
-            'created_at' => Carbon::now(),
-        ];
-
-        $post->fill($data);
-
-        $post->save();
-    }
-
-    private function parseSubject($result): string
-    {
-        if (preg_match('/#(.*?)\\n/', $result, $match) == 1) {
-            return $match[1];
-        }
-
-        return '';
     }
 }
---
+++ app/Jobs/RequestBlogPost.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class RequestBlogPost implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $result = '';
+        $result = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'system', 'content' => 'Generate an article about a feature of laravel. Provide your answer in markdown form. Reply with only the answer in markdown form. Add Extra Line breaks between paragraphs. Use code examples in markdown when needed.'],
+            ],
+        ]);
+
+        $result = $result['choices'][0]['message']['content'];
+
+        $post = new BlogPost();
+
+        $subject = '';
+
+        $subject = $this->parseSubject($result);
+
+        $subject = str_replace(['#', '*'], '', $subject);
+
+        $result = str_replace($subject, '', $result);
+
+        $data = [
+            'user_id' => 1,
+            'subject' => $subject,
+            'body' => $result,
+            'created_at' => Carbon::now(),
+        ];
+
+        $post->fill($data);
+
+        $post->save();
+    }
+
+    private function parseSubject($result): string
+    {
+        if (preg_match('/#(.*?)\\n/', $result, $match) == 1) {
+            return $match[1];
+        }
+
+        return '';
+    }
+}
